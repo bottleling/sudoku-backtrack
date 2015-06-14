5b9ebd611cd8ac8b5b80dde2a783a3eb4d699127
jean <jean@jean.com>
2015-06-14 21:50:16 +0800

    corrected range in hasConflict, added getters and setters for puzzle

--- backtrack.py
+++ backtrack.py
@@ -1,3 +1,4 @@
+import math
 class Node:
 	def __init__(self, value, domain=[1,2,3,4,5,6,7,8,9]):
 		self.value = value;
@@ -9,6 +10,14 @@ class Puzzle:
 	def __init__(self,filename):
 		self.puzzle = [[Node(None) for i in range(9)] for j in range(9)]
 		self.readFile(filename)
+	def setValue(self, x ,y ,value):
+		self.puzzle[x][y].value = value
+	def getValue(self, x, y):
+		return self.puzzle[x][y].value
+	def getDomain(self,x,y):
+		return self.puzzle[x][y].domain
+	def removeFromDomain(self,x,y,valToRemove):
+		self.puzzle[x][y].domain.remove(valToRemove)
 	def readFile(self, filename):
 		with open(filename, 'r') as f:
 			for i in range(9):
@@ -20,34 +29,39 @@ class Puzzle:
 					except:
 						continue
 
-	def hasConflict(rowIndex, colIndex):
-		val = self.puzzle[rowIndex][colIndex].value()
+	def hasConflict(self,rowIndex, colIndex):
+		val = self.puzzle[rowIndex][colIndex].value
 		#Check no value in this row has the same value as this assignment:
-			for j in range(0,9):
-				if j != colIndex:
-					if self.puzzle[rowIndex][j].value == val:
-						return True
+		for j in range(0,9):
+			if j != colIndex:
+				if self.puzzle[rowIndex][j].value == val:
+					return True
 
 		#Check no value in this column has the same value as this assignment:
-			for i in range(1,10):
-				if i != rowIndex:
-					if self.puzzle[i][colIndex].value == val:
-						return True
+		for i in range(0,9):
+			if i != rowIndex:
+				if self.puzzle[i][colIndex].value == val:
+					return True
 
 		#Check no value in this subsquare has the same value as this assignment:
-			boxR = math.floor(rowIndex/3) * 3 #row0-1's box will begin at row 0, row 3-5's box will begin at row 3. row 6-8's box will begin at row 6
-			boxC = math.floor(colIndex/3) * 3 #col0-1's box will begin at col 1...
-			for i in range(boxR, boxR+3): #check each row of the box
-				for j in range(boxC,boxC+3): #check each col of the box
-					if i == rowIndex and j == colIndex:
-						continue
-					else:
-						if self.puzzle[i][j].value == val:
-							return True
+		boxR = int (math.floor(rowIndex/3) * 3 ) #row0-1's box will begin at row 0, row 3-5's box will begin at row 3. row 6-8's box will begin at row 6
+		boxC = int( math.floor(colIndex/3) * 3 ) #col0-1's box will begin at col 1...
+		for i in range(boxR, boxR+3): #check each row of the box
+			for j in range(boxC,boxC+3): #check each col of the box
+				if i == rowIndex and j == colIndex:
+					continue
+				else:
+					if self.puzzle[i][j].value == val:
+						return True
 
-			return False
+		return False
 
 
 
 p = Puzzle("test/sudoku_easy.txt")
+p.setValue(2,0,7)
+print p.hasConflict(2,0)
+p.removeFromDomain(2,0,7)
 print p.puzzle
+p.setValue(2,0,4)
+print p.hasConflict(2,0)
