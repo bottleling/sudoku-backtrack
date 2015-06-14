39111e2b66edc702467e6b435506fa0e8ad45575
jean <jean@jean.com>
2015-06-14 23:43:52 +0800

    get most constrained coordinates

--- backtrack.py
+++ backtrack.py
@@ -10,11 +10,7 @@ class Puzzle:
 	def __init__(self,filename):
 		self.puzzle = [[Node(None,[1,2,3,4,5,6,7,8,9]) for i in range(9)] for j in range(9)]
 		self.readFile(filename)
-		self.alterAllDomains()
-	def setValue(self, x ,y ,value):
-		self.puzzle[x][y].value = value
-	def getValue(self, x, y):
-		return self.puzzle[x][y].value
+		self.alterAllDomains(self.puzzle)
 	def getDomain(self,x,y):
 		return self.puzzle[x][y].domain
 	def removeFromDomain(self,x,y,valToRemove):
@@ -57,17 +53,18 @@ class Puzzle:
 						return True
 
 		return False
-	def isComplete(self):
-		for node in self.puzzle:
+	
+	def isComplete(self, grid):
+		for node in grid:
 			if node.value == None:
 				return False
 		return True
 	
-	def alterAllDomains(self):
+	def alterAllDomains(self , grid):
 		values = set()
 		for i in range(0,9):
 			for j in range(0,9):
-				values.add(self.puzzle[i][j].value)
+				values.add(grid[i][j].value)
 			for j in range(0,9):
 				for x in values:
 					self.removeFromDomain(i,j,x)
@@ -75,7 +72,7 @@ class Puzzle:
 
 		for j in range(0,9):
 			for i in range(0,9):
-				values.add(self.puzzle[i][j].value)
+				values.add(grid[i][j].value)
 			for i in range(0,9):
 				for x in values:
 					self.removeFromDomain(i,j,x)
@@ -87,16 +84,31 @@ class Puzzle:
 				boxC = int( math.floor(j/3) * 3 ) #col0-1's box will begin at col 1...
 				for i in range(boxR, boxR+3): #check each row of the box
 					for j in range(boxC,boxC+3): #check each col of the box
-						values.add(self.puzzle[i][j].value)
+						values.add(grid[i][j].value)
 				#print "boxR:",boxR, " boxC:", boxC, values
 				for m in range(boxR,boxR+3):
 					for n in range(boxC,boxC+3):
 						for x in values:
 							self.removeFromDomain(m,n,x)
 				values.clear()
-		
-		
-		
+
+	def getMostConstrainedCoordinates(self): #unassigned variable with least number of values in its domain
+		coords=[]
+		size = 0
+		for i in range(9):
+			for j in range(9):
+				if self.puzzle[i][j].value == None:
+					if len(coords) == 0:
+						coords.append((i,j))
+						size = len(self.getDomain(i,j))
+					if len(self.getDomain(i,j))  == size:
+						coords.append((i,j))
+					if len(self.getDomain(i,j))  < size:
+						del coords[:]
+						size = len(self.getDomain(i,j))
+						coords.append((i,j))							
+
+		return coords #returns [(x,y),(x1,y1)...] if there are multiple unassigned nodes with the least number of values in their domain
 
 	def backtracking(assignment, value, domain, rowIndex, colIndex):
 		if assignment.is_complete:
@@ -107,10 +119,8 @@ class Puzzle:
 
 
 p = Puzzle("test/sudoku_easy.txt")
-print p.puzzle
-# p.setValue(2,0,7)
+print p.getMostConstrainedCoordinates()
 # print p.hasConflict(2,0)
 # p.removeFromDomain(2,0,7)
 # print p.puzzle
-# p.setValue(2,0,4)
 # print p.hasConflict(2,0)
